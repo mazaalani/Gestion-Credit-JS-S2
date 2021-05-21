c260e3631dc82853248bbc8bf854261e43585a50
mazaalani <72396085+mazaalani@users.noreply.github.com>
2021-05-21 12:04:04 -0400

    v1.0

--- js/GestionClients.class.js
+++ js/GestionClients.class.js
@@ -58,6 +58,8 @@
         }
         this.eClient.insertAdjacentHTML("beforeend", tHTML);
       }
+      //animation affichage client
+      animation_base(this.eClient, "apparait");
     });
 
     /* Affichez les données des prêts du client sélectionné dans la balise this.ePrets
@@ -65,6 +67,7 @@
 
     //remise a zero affichage
     this.ePrets.innerHTML = "";
+    this.ePrets.classList.remove("prets-box");
     //Affichage prets
     this.prets.forEach((pret) => {
       if (pret.id_client == clientSelect[2]) {
@@ -73,23 +76,37 @@
           tHTML = tHTML.replaceAll(`{${prop}}`, pret[prop]);
         }
         this.ePrets.insertAdjacentHTML("beforeend", tHTML);
+        this.ePrets.classList.add("prets-box");
       }
+      //animation affichage prets avec delais pour execution apres animation client
+      animation_base(this.ePrets, "apparait-move");
     });
 
     //Si le client sélectionné n'a aucun prêt, insérez par exemple "<p>Aucun prêt.</p>" dans la section this.ePrets
-    if (this.ePrets.innerHTML == "")
+    if (
+      this.ePrets.innerHTML == "" &&
+      this.eSelectClient.value != "Sélectionnez un client"
+    )
       this.ePrets.insertAdjacentHTML("beforeend", "<p>Aucun prêt.</p>");
 
-    /*  Si aucun client n'est sélectionné (option "Sélectionnez un client"), réinitialisez
-        les sections this.eClient et this.ePrets
-
-        
-
-        Animez l'affichage de ces sections this.eClient et this.ePrets
+    /*  Animez l'affichage de ces sections this.eClient et this.ePrets
         Vous pouvez utiliser par exemple la transition déclarée sur le parent d'id "wrapper"
-        ou bien définir toute(s) autre(s) transition(s) à votre convenance
-		
-        Vous pouvez organiser votre code en ajoutant des méthodes à cette méthode detailClient()     
+        ou bien définir toute(s) autre(s) transition(s) à votre convenance */
+
+    /**
+     * Fonction d'animation de base sur les elements en param
+     * @param {object, string} element cible, animation
+     */
+    function animation_base(cible, anim) {
+      cible.style.opacity = 0;
+      cible.classList.remove(anim);
+      setTimeout(() => cible.classList.add(anim), 5);
+      //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/animationend_event
+      cible.addEventListener("animationend", () => {
+        cible.style.opacity = 1;
+      });
+    }
+    /*  Vous pouvez organiser votre code en ajoutant des méthodes à cette méthode detailClient()     
            
     */
   }
